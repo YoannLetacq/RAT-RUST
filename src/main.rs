788d73d244d47532a4844dc668d78db43881ce6c
Yoann <117602278+YoannLetacq@users.noreply.github.com>
2024-08-22 07:59:02 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,21 @@ fn main() {
 
     library.display_books();
 
-    // Emprunter un livre.
     match library.borrow_by_title("1984") {
         Ok(_) => println!("Livre emprunté avec succès."),
         Err(e) => println!("Erreur: {}", e),
     }
 
-    // Essayer de réemprunter le même livre.
     match library.borrow_by_title("1984") {
         Ok(_) => println!("Livre emprunté avec succès."),
         Err(e) => println!("Erreur: {}", e),
     }
 
-    // Retourner le livre.
     match library.return_by_title("1984") {
         Ok(_) => println!("Livre retourné avec succès."),
         Err(e) => println!("Erreur: {}", e),
     }
 
-    // Afficher les livres après les opérations.
     library.display_books();
 
     // solution exos
